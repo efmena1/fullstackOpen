d7d818a1403ae987ff1be9404e1725181427504e
EDISON <eddy.908@hotmail.es>
2021-09-02 18:02:52 -0500

    exercise 2.13

--- part2/data_for_countries/src/index.js
+++ part2/data_for_countries/src/index.js
@@ -2,91 +2,112 @@ import React, { useState, useEffect } from "react";
 import ReactDOM from "react-dom";
 import axios from "axios";
 
-const Filter = ({state,handler}) =>{
-  return(
+const Filter = ({ state, handler }) => {
+  return (
     <div>
       filter countries
       <input value={state} onChange={handler} />
     </div>
-  )
-}
+  );
+};
 
-const DisplayData = ({countries}) =>{
-  if(countries.length >10){
-    return(
-      <div>
-        Too many matches, specify another filter
-      </div>
-    )
-  }
-  if(countries.length === 1){
-    var flagStyle = {
-      "width": "120px",
-      "height": "120px",
-   };
-    return(
-      <div>
-        {countries.map((country) => 
-        <div key={country.name}>
-          <h1>{country.name}</h1>
-          <h2>languages</h2>
-          <ul>{country.languages.map((language)=>
-            <li key={language.name}><b>{language.name}</b></li>
-          )}
-          </ul>
-          <img src={country.flag} alt="Flag" style={flagStyle}/>
-        </div>
-      )}
-      </div>
-    )
-  }
-  if(countries.length=== 0 ) return (<div>No match found</div>)
-  return(
-    <div>
-      <h2>Countries</h2>
+const Button = ({ onClick, text, name }) => {
+  return (
+    <>
+      <button onClick={onClick} name={name}>
+        {text}
+      </button>
+    </>
+  );
+};
+
+const DisplayCountry = ({ countries }) => {
+  var flagStyle = {
+    width: "120px",
+    height: "120px",
+  };
+  const country = countries[0];
+  return (
+    <div key={country.name}>
+      <h1>{country.name}</h1>
+      <h2>languages</h2>
       <ul>
-      {countries.map((country) => 
-        <li key={country.name}>{country.name}</li>
-      )}
+        {country.languages.map((language) => (
+          <li key={language.name}>
+            <b>{language.name}</b>
+          </li>
+        ))}
+      </ul>
+      <img src={country.flag} alt="Flag" style={flagStyle} />
+    </div>
+  );
+};
+
+const DisplayData = ({ countries }) => {
+  const [show, setShow] = useState("");
+
+  const handleShowClick = (event) => {
+    const showObject = countries.filter(
+      (country) => country.name === event.target.name
+    );
+    setShow(showObject);
+  };
+  if (countries.length === 0) return <p>Enter a keyword to filter</p>;
+  if (countries.length > 10) return <p>Too many matches</p>;
+  if (countries.length === 1) return <DisplayCountry countries={countries} />;
+  return (
+    <div>
+      <ul>
+        {countries.map((country) => {
+          return (
+            <li key={country.name}>
+              {country.name}
+              <Button
+                onClick={handleShowClick}
+                name={country.name}
+                text="show"
+              />
+              {show && country.name === show[0].name ? (
+                  <DisplayCountry countries={show} />
+              ) : null}
+            </li>
+          );
+        })}
       </ul>
     </div>
-  )
-}
+  );
+};
 
-const App = () =>{
+const App = () => {
   const [countries, setCountries] = useState([]);
-  const [filter,setFilter] = useState('')
-
+  const [filter, setFilter] = useState("");
   useEffect(() => {
-    console.log('effect')
-    axios
-      .get('https://restcountries.eu/rest/v2/all')
-      .then(response => {
-        console.log('promise fulfilled')
-        setCountries(response.data)
-      })
-  }, [])
+    console.log("effect");
+    axios.get("https://restcountries.eu/rest/v2/all").then((response) => {
+      console.log("promise fulfilled");
+      setCountries(response.data);
+    });
+  }, []);
 
   const handleFilterChange = (event) => {
     setFilter(event.target.value);
-  }
+  };
 
-  const FilteredCountries = (filter,countries) =>{
-    if(filter === '') return []
-    return countries.filter(function (country){
-      const re = RegExp(`.*${filter.toLowerCase()}.*`)
-      return country.name.toLowerCase().match(re)
-    })
-  }
+  const FilteredCountries = (filter, countries) => {
+    if (filter === "") return [];
+    return countries.filter((country) => {
+      const re = RegExp(`.*${filter.toLowerCase()}.*`);
+      return country.name.toLowerCase().match(re);
+    });
+  };
 
-
-  return(
+  return (
     <div>
       <h1>Data for Countries</h1>
       <Filter state={filter} handler={handleFilterChange} />
-      <DisplayData countries={FilteredCountries(filter,countries)} />
+      <DisplayData countries={FilteredCountries(filter, countries)} />
     </div>
-  )
-}
+  );
+};
 
 ReactDOM.render(<App />, document.getElementById("root"));
