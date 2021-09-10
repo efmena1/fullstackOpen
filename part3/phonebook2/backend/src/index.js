62e356e3e8362647d2eda3eb240d6b4e3a7b9167
EDISON <eddy.908@hotmail.es>
2021-09-10 15:57:32 -0500

    exercise 3.18

--- part3/phonebook2/backend/src/index.js
+++ part3/phonebook2/backend/src/index.js
@@ -49,13 +49,11 @@ app.get("/api/persons", (request, response) => {
 });
 
 app.get("/api/persons/:id", (request, response) => {
-  const id = Number(request.params.id);
-  const person = persons.find((person) => person.id === id);
-  if (person) {
-    return response.json(person);
-  } else {
-    return response.status(404).end("Person not found");
-  }
+  Person.findById(request.params.id)
+    .then((person) => {
+      response.json(person);
+    })
+    .catch((error) => next(error));
 });
 
 app.post("/api/persons", (request, response) => {
