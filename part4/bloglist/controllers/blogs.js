a010bb29f8497d10a0d6f9cfcf6f59240f118851
EDISON <eddy.908@hotmail.es>
2021-09-13 22:06:54 -0500

    exercise 4.21

--- part4/bloglist/controllers/blogs.js
+++ part4/bloglist/controllers/blogs.js
@@ -4,23 +4,13 @@ const jwt = require('jsonwebtoken');
 const Blog = require('../models/blog');
 const User = require('../models/user');
 
-const getTokenFrom = (request) => {
-  const authorization = request.get('authorization');
-  if (authorization && authorization.toLowerCase().startsWith('bearer ')) {
-    return authorization.substring(7);
-  }
-  return null;
-};
-
 blogsRouter.get('/', async (request, response) => {
   const blogs = await Blog.find({}).populate('user', { username: 1, name: 1 });
   response.json(blogs);
 });
 
 blogsRouter.post('/', async (request, response) => {
-  const { body } = request;
-
-  const token = getTokenFrom(request);
+  const { body, token } = request;
   const decodedToken = jwt.verify(token, process.env.SECRET);
   if (!token || !decodedToken.id) {
     return response.status(401).json({ error: 'token missing or invalid' });
@@ -54,11 +44,28 @@ blogsRouter.post('/', async (request, response) => {
 });
 
 blogsRouter.delete('/:id', async (request, response) => {
-  const deletedBlog = await Blog.findByIdAndDelete(request.params.id);
-  if (deletedBlog) {
+  const { token } = request;
+
+  const decodedToken = jwt.verify(token, process.env.SECRET);
+  if (!token || !decodedToken.id) {
+    return response.status(401).json({ error: 'token missing or invalid' });
+  }
+
+  const blogToDelete = await Blog.findById(request.params.id);
+  if (!blogToDelete) {
+    return response.status(404).json({
+      error: 'blog not found',
+    });
+  }
+  const userId = blogToDelete.user;
+  if (userId.toString() === decodedToken.id.toString()) {
+    await blogToDelete.delete();
+    await User.updateOne({ blogs: request.params.id }, { $pull: { blogs: request.params.id } });
     return response.status(204).end();
   }
-  return response.status(404).end();
+  return response.status(401).json({
+    error: 'invalid user',
+  });
 });
 
 blogsRouter.put('/:id', async (request, response) => {
