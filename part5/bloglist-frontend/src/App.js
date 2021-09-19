44b7e832b2f7874763cca1473ffcf6c5e76b64b0
EDISON <eddy.908@hotmail.es>
2021-09-19 13:31:29 -0500

    exercise 5.1-5.2

---
+++ part5/bloglist-frontend/src/App.js
@@ -0,0 +1,79 @@
+import React, { useState, useEffect } from 'react'
+import Blog from './components/Blog'
+import LoginForm from './components/Form'
+
+import blogService from './services/blogs'
+import loginService from './services/login'
+
+const App = () => {
+  const [blogs, setBlogs] = useState([])
+
+  const [username, setUsername] = useState('')
+  const [password, setPassword] = useState('')
+
+  const [user, setUser] = useState(null)
+
+  useEffect(() => {
+    blogService.getAll().then(blogs =>
+      setBlogs( blogs )
+    )  
+  }, [])
+
+  useEffect(() => {
+    const loggedUserJSON = window.localStorage.getItem('loggedBlogAppUser')
+    if (loggedUserJSON) {
+      const user = JSON.parse(loggedUserJSON)
+      setUser(user)
+    }
+  }, [])
+
+  const loginHandle = async (event) =>{
+    event.preventDefault()
+    try{
+      const user = await loginService.login({
+        username,password
+      })
+      window.localStorage.setItem(
+        'loggedBlogAppUser', JSON.stringify(user)
+      )
+      setUser(user)
+      setUsername('')
+      setPassword('') 
+    } catch (exeption) {
+      console.log('Wrong Credentials')
+    }
+  }
+
+  const logoutHandle = async (event) =>{
+    window.localStorage.clear()
+    setUser(null)
+  }
+  if (user === null) {
+    return(
+      <div>
+        <h2>Log in to application</h2>
+        <LoginForm
+          username = {username}
+          userhandle = {setUsername}
+          password = {password}
+          passwordHandle = {setPassword}
+          loginHandle = {loginHandle}
+        />
+      </div>
+    )
+  }
+  return (
+    <div>
+      <h2>blogs</h2>
+      <p>
+        <b>{user.name} logged in</b>
+        <button type='button' onClick={() => logoutHandle()}>logout</button>
+      </p>
+      {blogs.map(blog =>
+        <Blog key={blog.id} blog={blog} />
+      )}
+    </div>
+  )
+}
+
+export default App
